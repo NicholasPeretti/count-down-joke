06fcba680cb2a0178416f09701f078ac7bd67a17
Nicholas Peretti <peretti.nicholas@gmail.com>
2019-02-09 14:06:09 +0100

    Updates

---
+++ precache-manifest.9df6f284adf59c7df165773ef3221f5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d98659284083fafbb01",
+    "url": "/count-down-joke/static/css/main.cbd410a9.chunk.css"
+  },
+  {
+    "revision": "4d98659284083fafbb01",
+    "url": "/count-down-joke/static/js/main.4d986592.chunk.js"
+  },
+  {
+    "revision": "c3636c9d18b7b48eab3c",
+    "url": "/count-down-joke/static/js/1.c3636c9d.chunk.js"
+  },
+  {
+    "revision": "ec1594431134311e11bc",
+    "url": "/count-down-joke/static/js/runtime~main.ec159443.js"
+  },
+  {
+    "revision": "da5b14fce2207410b12237b56468df20",
+    "url": "/count-down-joke/index.html"
+  }
+];
