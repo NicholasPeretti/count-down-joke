81ccf8621892679c6f5b0e52faa15c9006cfd6bc
Nicholas Peretti <peretti.nicholas@gmail.com>
2019-02-09 14:00:44 +0100

    Create timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import moment from 'moment'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      targetdate: moment('2019-02-09 17:00'),
+      currentDate: new Date(),
+      intervalRef: null
+    }
+  }
+
+  componentDidMount () {
+    if (this.state.intervalRef) return
+
+    let intervalRef = setInterval(() => {
+      this.setState({
+        currentDate: new Date()
+      })
+    }, 1000)
+    this.setState({
+      intervalRef
+    })
+  }
+
   render() {
+    const { currentDate, targetdate } = this.state
+    const diff = targetdate.diff(currentDate, 'seconds')
+    const missingTime = moment.duration(diff, 'seconds')
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 style={{ fontSize: '100px' }}>
+            {(diff < 0) ?
+              `Time's up!`
+            :
+              `${missingTime.hours()}H ${missingTime.minutes()}m ${missingTime.seconds()}s`
+            }
+          </h1>
         </header>
       </div>
     );
